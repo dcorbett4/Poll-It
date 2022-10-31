3f95eec1b93e5e771c89aac9b307d752a1dd263a
b-l-u <98289926+b-l-u@users.noreply.github.com>
2022-10-31 10:26:25 -0400

    Upload And Display Image, Bug Fixes

---
+++ VoteSystem/userpages/homepage_user.php
@@ -0,0 +1,53 @@
+<?php
+
+session_start();
+$user = $_SESSION['user'];
+$id = $_SESSION['id'];
+
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Home Page Title</title>
+        <link rel="stylesheet" type="text/css" href="/VoteSystem/StyleS/Homepage.css" /> 
+    </head>
+    
+    <body>
+    <h2>Welcome, <?php echo $user?></h2>
+
+    <div>
+        <img id="profile_pic" src ="/VoteSystem/User_Imgs/defaultimg.jpg" alt="Default_User Image">
+    </div>
+
+    <div>
+    <h3>HOME</h3>
+        <nav>
+            <ul>
+
+                <li><a href="/VoteSystem/homepage_user.php"> DASHBOARD</a></li>
+                <li><a href="#"> RESULTS</a></li>
+
+            </ul>
+
+        </nav>
+
+        <h3>MANAGE</h3>
+        <nav>
+            <ul>
+                <li><a href="#"> POLL </a></li>   
+            </ul>
+        </nav>
+
+        <h3>SETTINGS</h3>
+        <nav>
+            <ul>
+                <li><a href="#"> USER </a></li>
+            </ul>
+        </nav>
+        <form action="/VoteSystem/logout.php" method="POST"> 
+      <button type="submit" name="logout" class="btn btn-primary">Logout</button>
+        </form>
+    </div>
+
+    </body>
+</html>
