67c3318cb03bd6a449ef3480be8fd7c70fd77974
Dylan Corbett <dylan.corbett24@gmail.com>
2022-11-15 19:32:54 -0500

    Fixing createpoll to properly work

--- VoteSystem/FormHandlers/action_createpoll.php
+++ VoteSystem/FormHandlers/action_createpoll.php
@@ -10,13 +10,10 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $poll_name = $_POST['title'];
     $erro = [];
     $date = date('Y-m-d H:i:s');
-    $enddate = date('Y-m-d H:i:s' ,strtotime(' + 1 days'));
-    
+    $enddate = date('Y-m-d H:i:s', strtotime(' + 1 days'));
+
     $sql = "SELECT * FROM poll";
     $query = mysqli_query($conn, $sql);
-
-    
-    if(mysqli_num_rows($query) == 0){
 
     if (empty($poll_name)) {
         $_SESSION['reg_err'][] = "Title is Empty<br>";
@@ -31,7 +28,6 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         }
     }
 
-    }
     echo "Poll Already Created Reset To Create A New Poll";
 }
 
