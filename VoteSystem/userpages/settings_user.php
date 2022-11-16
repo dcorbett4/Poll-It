061ff41cbc888b0d8085c30ef798c744aee2637e
Dylan Corbett <dylan.corbett24@gmail.com>
2022-11-16 15:19:53 -0500

    Added the basic function of user voting

--- VoteSystem/userpages/settings_user.php
+++ VoteSystem/userpages/settings_user.php
@@ -39,7 +39,7 @@ include('C:/xampp/htdocs/VoteSystem/Helpers.php');
         <h3>MANAGE</h3>
         <nav>
             <ul>
-                <li><a href="#"> POLL </a></li>
+                <li><a href="/VoteSystem/userpages/vote_user.php"> VOTE </a></li>
             </ul>
         </nav>
 
