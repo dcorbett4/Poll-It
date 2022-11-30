cd5ee5e9f097ad726cfd771b06760d431e3d5325
Dylan Corbett <dylan.corbett24@gmail.com>
2022-11-30 13:13:19 -0500

    added a function to check expiration date

--- VoteSystem/adminpages/homepage.php
+++ VoteSystem/adminpages/homepage.php
@@ -4,25 +4,27 @@ session_start();
 $user = $_SESSION['user'];
 $id = $_SESSION['id'];
 include('C:/xampp/htdocs/VoteSystem/Helpers.php');
+require('C:/xampp/htdocs/VoteSystem/Connection.php');
 
 ?>
 <!DOCTYPE html>
 <html>
-    <head>
-        <title>Home Page Title</title>
-        <link rel="stylesheet" type="text/css" href="/VoteSystem/StyleS/Homepage.css" /> 
-    </head>
-    
-    <body>
-    <h2>Welcome, <?php echo $user?></h2>
+
+<head>
+    <title>Home Page Title</title>
+    <link rel="stylesheet" type="text/css" href="/VoteSystem/StyleS/Homepage.css" />
+</head>
+
+<body>
+    <h2>Welcome, <?php echo $user ?></h2>
 
     <?php
     profile_pic();
-    
+
     ?>
 
     <div>
-    <h3>HOME</h3>
+        <h3>HOME</h3>
         <nav>
             <ul>
 
@@ -40,7 +42,7 @@ include('C:/xampp/htdocs/VoteSystem/Helpers.php');
                 <li><a href="/VoteSystem/adminpages/Manage_Polls.php"> POLL </a></li>
                 <li><a href="/VoteSystem/adminpages/Manage_Choices.php"> CHOICES</a></li>
                 <li><a href="/VoteSystem/adminpages/Manage_Voters.php"> VOTERS</a></li>
-                
+
 
             </ul>
         </nav>
@@ -52,13 +54,26 @@ include('C:/xampp/htdocs/VoteSystem/Helpers.php');
             </ul>
         </nav>
 
-        <form action="/VoteSystem/logout.php" method="POST"> 
-      <button type="submit" name="logout" class="btn btn-primary">Logout</button>
+        <form action="/VoteSystem/logout.php" method="POST">
+            <button type="submit" name="logout" class="btn btn-primary">Logout</button>
         </form>
     </div>
 
-    <h2><a href="/VoteSystem/adminpages/Manage_Polls.php">No Poll Created Go To Poll Page</a></h2>
+    <h2><a href="/VoteSystem/adminpages/Manage_Polls.php">
+            <?php
+            $check = checkexpiration();
+
+            if ($check == 2) {
+                echo "No Poll Created";
+            } else if ($check == 1) {
+                echo "Poll has Expired";
+            } else if ($check == 0) {
+                echo "Poll is Still Available";
+            }
+            ?>
+        </a></h2>
 
 
-    </body>
+</body>
+
 </html>
